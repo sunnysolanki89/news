95f0856c7739eba6155a94006ef74da1bc61ea91
unknown <chandrapratap.solanki@gmail.com>
2020-01-12 10:13:34 +0530

    first commit

---
+++ src/app/news/news.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.page.html',
+  styleUrls: ['./news.page.scss'],
+})
+export class NewsPage implements OnInit {
+data: any;
+  constructor(private newsService: NewsService, private router: Router) { }
+
+  ngOnInit() {
+    this.newsService.getData('top-headlines?country=us&category=business').subscribe(data => {
+      //console.log(data);
+      this.data = data;
+    });
+  }
+  onGoToSinglePage(article){
+    this.newsService.currentArticle = article;
+    this.router.navigate(['/news-single'])
+  }
+}
